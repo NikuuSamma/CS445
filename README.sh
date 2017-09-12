fc3c865a67671fddc8576dc0d0d44b66ac7e861f
nikuusamma <nickgarcialara@gmail.com>
2017-09-12 11:09:01 -0500

    remove duplicate file

--- README.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash  
-echo "This is a shell script to compile/run java code for this project"  
-cd HW1
-cd homework
-javac *.java
-cd ../
-java homework/TestCreature
-echo "I am done running ls"  
-
